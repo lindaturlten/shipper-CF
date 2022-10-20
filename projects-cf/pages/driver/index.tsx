bd9c98bfd5e5f60237d0c0ac18ed2596191a8fa7
lindaturlten <lindaturlten@gmail.com>
2022-10-21 04:26:22 +0700

    UI Menus, header, and layout

---
+++ projects-cf/pages/driver/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import DriverManagementContainer from '../../containers/driver';
+
+const DriverManagement: NextPage = () => {
+  return <DriverManagementContainer />;
+};
+
+export default DriverManagement;
